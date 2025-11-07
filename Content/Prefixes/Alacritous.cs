76d2e9b84b9d1665e79d9dbf08246c4a9f977b9d
MajOfMyth <connormcgrath06@gmail.com>
2025-11-07 02:10:30 -0600

    First Prefixes

---
+++ Content/Prefixes/Alacritous.cs
@@ -0,0 +1,37 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace ReforgesReforged.Content.Prefixes
+{
+    public class Alacritous : ModPrefix
+    {
+        public override PrefixCategory Category => PrefixCategory.Ranged;
+
+        public override float RollChance(Item item)
+        {
+            return 1f;
+        }
+
+        public override bool CanRoll(Item item)
+        {
+            return true;
+        }
+
+        public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult, ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus)
+        {
+            damageMult *= 0.5f;
+            useTimeMult *= 0.4f;
+        }
+
+        public override void ModifyValue(ref float valueMult)
+        {
+            valueMult *= 1.25f;
+        }
+
+        public override void Apply(Item item)
+        {
+
+        }
+
+    }
+}
---
+++ Content/Prefixes/Vacuous.cs
@@ -0,0 +1,37 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace ReforgesReforged.Content.Prefixes
+{
+    public class Vacuous : ModPrefix
+    {
+        public override PrefixCategory Category => PrefixCategory.Magic;
+
+        public override float RollChance(Item item)
+        {
+            return 1f;
+        }
+
+        public override bool CanRoll(Item item)
+        {
+            return true;
+        }
+
+        public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult, ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus)
+        {
+            manaMult *= 0f;
+            damageMult *= 0.5f;
+        }
+
+        public override void ModifyValue(ref float valueMult)
+        {
+            valueMult *= 1.25f;
+        }
+
+        public override void Apply(Item item)
+        {
+
+        }
+
+    }
+}
