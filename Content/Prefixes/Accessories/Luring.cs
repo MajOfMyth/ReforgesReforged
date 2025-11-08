2b37f6fe75525d71b860aa17ad35d835b0d30326
Faolan1216 <kameronidarling@gmail.com>
2025-11-08 00:49:12 -0800

    Luring
    Luring is a fishing prefix

---
+++ Content/Prefixes/Accessories/Luring.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace ReforgesReforged.Content.Prefixes.Accessories
+{
+    public class Luring : ModPrefix
+    {
+
+        public override PrefixCategory Category => PrefixCategory.Accessory;
+
+        public override float RollChance(Item item)
+        {
+            return 1f;
+        }
+
+        public override bool CanRoll(Item item)
+        {
+            return true;
+        }
+
+        public override void ApplyAccessoryEffects(Player player)
+        {
+            player.fishingSkill += 5;
+        }
+
+        public override void ModifyValue(ref float valueMult)
+        {
+            valueMult *= 1.25f;
+        }
+
+        public override IEnumerable<TooltipLine> GetTooltipLines(Item item)
+        {
+
+            yield return new TooltipLine(Mod, "FishingTooltip", "+5 " + FishingTooltip.Value)
+            {
+                IsModifier = true,
+            };
+
+        }
+
+        public LocalizedText FishingTooltip => Mod.GetLocalization($"Fishing");
+
+    }
+}
