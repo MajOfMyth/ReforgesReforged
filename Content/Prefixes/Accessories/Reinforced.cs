e2703d51a130392d36b4f7caa14d604374dacac6
MajOfMyth <connormcgrath06@gmail.com>
2025-11-08 01:36:19 -0600

    Reinforced 🅰️

---
+++ Content/Prefixes/Accessories/Reinforced.cs
@@ -0,0 +1,53 @@
+﻿using ReforgesReforged.Common.ModPlayers;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace ReforgesReforged.Content.Prefixes.Accessories
+{
+    public class Reinforced : ModPrefix
+    {
+
+        public override PrefixCategory Category => PrefixCategory.Accessory;
+
+        public override float RollChance(Item item)
+        {
+            return 1f;
+        }
+
+        public override bool CanRoll(Item item)
+        {
+            return true;
+        }
+
+        public override void ApplyAccessoryEffects(Player player)
+        {
+            player.endurance += 0.05f;
+            player.GetModPlayer<RRPlayer>().SpeedMult *= 0.9f;
+        }
+
+        public override void ModifyValue(ref float valueMult)
+        {
+            valueMult *= 1.25f;
+        }
+
+        public override IEnumerable<TooltipLine> GetTooltipLines(Item item)
+        {
+
+            yield return new TooltipLine(Mod, "EnduranceTooltip", "+5% " + EnduranceTooltip.Value)
+            {
+                IsModifier = true,
+            };
+            yield return new TooltipLine(Mod, "SpeedTooltip", "-10% " + SpeedTooltip.Value)
+            {
+                IsModifier = true,
+                IsModifierBad = true,
+            };
+        }
+
+        public LocalizedText EnduranceTooltip => Mod.GetLocalization($"Endurance");
+        public LocalizedText SpeedTooltip => Mod.GetLocalization($"MovementSpeed");
+
+    }
+}
