6606d1404ef2a63bea8e39e93d13b2623b037662
Faolan1216 <kameronidarling@gmail.com>
2025-11-08 19:10:11 -0800

    Hydrating

---
+++ Content/Prefixes/Accessories/Hydrating.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using ThoriumMod;
+
+namespace ReforgesReforged.Content.Prefixes.Accessories
+{
+    [JITWhenModsEnabled("ThoriumMod")]
+    public class Hydrating : ModPrefix
+    {
+
+        public override PrefixCategory Category => PrefixCategory.Accessory;
+
+        public override float RollChance(Item item)
+        {
+            return 1f;
+        }
+
+        public override bool CanRoll(Item item)
+        {
+            return true;
+        }
+
+        public override void ApplyAccessoryEffects(Player player)
+        {
+            player.GetModPlayer<ThoriumPlayer>().throwerExhaustionMax += 120;
+            player.GetModPlayer<ThoriumPlayer>().throwerExhaustionRegenBonus += 0.1f;
+        }
+
+        public override void ModifyValue(ref float valueMult)
+        {
+            valueMult *= 1.25f;
+        }
+
+        public override IEnumerable<TooltipLine> GetTooltipLines(Item item)
+        {
+
+            yield return new TooltipLine(Mod, "MaxExhaustionTooltip", "+10% " + MaxExhaustionTooltip.Value)
+            {
+                IsModifier = true,
+            };
+            yield return new TooltipLine(Mod, "ExhaustionRegenTooltip", "+10% " + ExhaustionRegenTooltip.Value)
+            {
+                IsModifier = true,
+            };
+
+        }
+
+        public LocalizedText MaxExhaustionTooltip => Mod.GetLocalization($"MaxExhaustion");
+        public LocalizedText ExhaustionRegenTooltip => Mod.GetLocalization($"ExhaustionRegen");
+
+    }
+}
