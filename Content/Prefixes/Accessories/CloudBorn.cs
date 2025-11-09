b84fc3e21e5bb134a10623e42703900769153a8a
Faolan1216 <kameronidarling@gmail.com>
2025-11-08 21:06:16 -0800

    Renamed Aerodynamic and added Soaring

---
+++ Content/Prefixes/Accessories/CloudBorn.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace ReforgesReforged.Content.Prefixes.Accessories
+{
+    public class CloudBorn : ModPrefix
+    {
+
+        public override PrefixCategory Category => PrefixCategory.Accessory;
+
+        public override float RollChance(Item item)
+        {
+            return 1f;
+        }
+
+        public override bool CanRoll(Item item)
+        {
+            if (item.wingSlot > -1) return true;
+            return false;
+        }
+
+        public override void ApplyAccessoryEffects(Player player)
+        {
+            player.wingTime += 0.1f;
+        }
+
+        public override void ModifyValue(ref float valueMult)
+        {
+            valueMult *= 1.25f;
+        }
+
+        public override IEnumerable<TooltipLine> GetTooltipLines(Item item)
+        {
+
+            yield return new TooltipLine(Mod, "FlightTime", "+10% " + FlightTimeTooltip.Value)
+            {
+                IsModifier = true,
+            };
+
+        }
+
+        public LocalizedText FlightTimeTooltip => Mod.GetLocalization($"FlightTime");
+
+    }
+}
---
+++ Content/Prefixes/Accessories/Soaring.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace ReforgesReforged.Content.Prefixes.Accessories
+{
+    public class Soaring : ModPrefix
+    {
+
+        public override PrefixCategory Category => PrefixCategory.Accessory;
+
+        public override float RollChance(Item item)
+        {
+            return 1f;
+        }
+
+        public override bool CanRoll(Item item)
+        {
+            if (item.wingSlot > -1) return true;
+            return false;
+        }
+
+        public override void ApplyAccessoryEffects(Player player)
+        {
+            player.empressBrooch = true;
+            player.GetDamage(DamageClass.Generic) *= 0f;
+        }
+
+        public override void ModifyValue(ref float valueMult)
+        {
+            valueMult *= 1.25f;
+        }
+
+        public override IEnumerable<TooltipLine> GetTooltipLines(Item item)
+        {
+
+            yield return new TooltipLine(Mod, "InfiniteFlight", "Infinite flight")
+            {
+                IsModifier = true,
+            };
+            yield return new TooltipLine(Mod, "NoDamage", "No Damage")
+            {
+                IsModifier = true,
+                IsModifierBad = true,
+            };
+
+        }
+    }
+}
