7dc46cd28140efbbbe8ddd3e5c41bf372e805901
MajOfMyth <connormcgrath06@gmail.com>
2025-11-08 23:16:44 -0600

    Redundant Reforge Removal + Option

--- Common/GlobalItems/RRGlobalItem.cs
+++ Common/GlobalItems/RRGlobalItem.cs
@@ -1,11 +1,81 @@
 ﻿using ReforgesReforged.Content.Prefixes.Weapons;
+using System.Linq;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace ReforgesReforged.Common.GlobalItems
 {
     internal class RRGlobalItem : GlobalItem
     {
+
+        public static int[] IllegalPrefixes = [
+            PrefixID.Hard,
+            PrefixID.Guarding,
+            PrefixID.Armored,
+
+            PrefixID.Precise,
+
+            PrefixID.Jagged,
+            PrefixID.Spiked,
+            PrefixID.Angry,
+
+            PrefixID.Brisk,
+            PrefixID.Fleeting,
+            PrefixID.Hasty2,
+
+            PrefixID.Wild,
+            PrefixID.Rash,
+            PrefixID.Intrepid,
+
+
+            PrefixID.Keen,
+            PrefixID.Superior,
+            PrefixID.Forceful,
+            PrefixID.Damaged,
+            PrefixID.Shoddy,
+            PrefixID.Hurtful,
+            PrefixID.Strong,
+            PrefixID.Unpleasant,
+
+            PrefixID.Quick,
+            PrefixID.Nimble,
+            PrefixID.Murderous,
+            PrefixID.Slow,
+            PrefixID.Lazy,
+            PrefixID.Annoying,
+
+            PrefixID.Large,
+            PrefixID.Dangerous,
+            PrefixID.Savage,
+            PrefixID.Sharp,
+            PrefixID.Pointy,
+            PrefixID.Terrible,
+            PrefixID.Small,
+            PrefixID.Unhappy,
+            PrefixID.Bulky,
+            PrefixID.Shameful,
+
+            PrefixID.Hasty,
+            PrefixID.Intimidating,
+            PrefixID.Staunch,
+            PrefixID.Lethargic,
+            PrefixID.Awkward,
+
+            PrefixID.Mystic,
+            PrefixID.Adept,
+            PrefixID.Inept,
+            PrefixID.Deranged,
+            PrefixID.Intense,
+            PrefixID.Taboo,
+        ];
+
+        public override bool AllowPrefix(Item item, int pre)
+        {
+            if (RRConfig.Instance.RemoveRedundant && IllegalPrefixes.Contains(pre)) return false;
+            return true;
+        }
+
 
         public override void ModifyWeaponDamage(Item item, Player player, ref StatModifier damage)
         {
---
+++ RRConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader.Config;
+
+namespace ReforgesReforged
+{
+    public class RRConfig : ModConfig
+    {
+        public static RRConfig Instance;
+
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+        //public override bool AcceptClientChanges(ModConfig pendingConfig, int whoAmI, ref string message) => true;
+
+        [DefaultValue(true)]
+        public bool RemoveRedundant { get; set; }
+    }
+}
