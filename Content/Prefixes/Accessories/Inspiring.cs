2d22df762a289c742a94f0cb6e8b372ce247dd98
Faolan1216 <kameronidarling@gmail.com>
2025-11-08 18:53:19 -0800

    Inspiring and Thorium weakReference

---
+++ Content/Prefixes/Accessories/Inspiring.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using ThoriumMod;
+
+namespace ReforgesReforged.Content.Prefixes.Accessories
+{
+    [JITWhenModsEnabled("ThoriumMod")]
+    public class Inspiring : ModPrefix
+    {
+
+        public override PrefixCategory Category => PrefixCategory.Accessory;
+
+        public override float RollChance(Item item)
+        {
+            return 1f;
+        }
+
+        public override bool CanRoll(Item item)
+        {
+            return true;
+        }
+
+        public override void ApplyAccessoryEffects(Player player)
+        {
+            player.GetModPlayer<ThoriumPlayer>().bardResourceMax2 += 1;
+        }
+
+        public override void ModifyValue(ref float valueMult)
+        {
+            valueMult *= 1.25f;
+        }
+
+        public override IEnumerable<TooltipLine> GetTooltipLines(Item item)
+        {
+
+            yield return new TooltipLine(Mod, "InspirationTooltip", "+1 " + InspirationTooltip.Value)
+            {
+                IsModifier = true,
+            };
+
+        }
+
+        public LocalizedText InspirationTooltip => Mod.GetLocalization($"MaxInspiration");
+
+    }
+}
